52b49a2fa6b1535c1cd65abcc6e78a40df803db9
unknown <ladoucar@ENAC-IHM17.ihm>
2015-01-28 14:21:14 +0100

    Correction de la machine a etat avec l'etat MENU

--- MarkingMenu/src/MainFrame.java
+++ MarkingMenu/src/MainFrame.java
@@ -28,7 +28,7 @@ public class MainFrame extends javax.swing.JFrame {
         s.addSection("7", new Color(0xcdd7e3));
         s.addSection("8", new Color(0xb6cae3));
         
-        MarkingPieMenuModel s2 = new MarkingPieMenuModel(jButton1);
+        MarkingPieMenuModel s2 = new MarkingPieMenuModel(jPanel1);
         s2.addSection("1", new Color(0x9bbf8b));
         s2.addSection("2", new Color(0x7fb866));
         s2.addSection("3", new Color(0xc7ddbe));
@@ -44,27 +44,57 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("jButton1");
+        jScrollPane1.setBackground(new java.awt.Color(0, 0, 0));
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setBackground(new java.awt.Color(102, 102, 102));
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Monospaced", 0, 12)); // NOI18N
+        jTextArea1.setForeground(new java.awt.Color(209, 233, 192));
+        jTextArea1.setRows(5);
+        jTextArea1.setText("> Commande : ");
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jPanel1.setBackground(new java.awt.Color(179, 223, 179));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 119, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 124, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(539, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(40, 40, 40))
+                .addContainerGap(501, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(400, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(39, 39, 39))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 244, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -106,6 +136,8 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
--- MarkingMenu/src/MarkingPieMenuModel.java
+++ MarkingMenu/src/MarkingPieMenuModel.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
@@ -29,60 +28,69 @@ import javax.swing.SwingUtilities;
  * @author ladoucar
  */
 /*newpopup = PopupFactory.getSharedInstace().getPopup(invoker, composantToPopup,x,y);
-newpopup.show();
-newpopup.hide();*/
+ newpopup.show();
+ newpopup.hide();*/
+public class MarkingPieMenuModel implements MouseMotionListener, MouseListener {
 
-public class MarkingPieMenuModel implements MouseMotionListener, MouseListener{
-    
     private static boolean DEBUGMODE = true;
-    
+
     public int INNERCIRCLESIZE = 40;
     public int OUTERCIRCLESIZE = 100;
     private static int WAITTIME = 1000;
-    
+
     private boolean in;
-    enum STATEMENU { NOMENU, WAIT, PIESHOW, PIEHIDE };
+
+    enum STATEMENU {
+
+        IDLE, MARKING, SHOW, HIDE, MENU
+    };
     STATEMENU state;
     Timer t;
     Point firstPoint;
-    
+
     private MouseEvent gMouse;
     private Component myComponent;
     private Component myRootComponent;
     private javax.swing.Popup myPopup;
-    
+
     private ArrayList<Section> sections;
     private Section selectedSection;
     private MarkingPieMenuViewTest myPieView;
-    
-     public MarkingPieMenuModel(Component c) {
-         sections = new ArrayList<>();
-         myComponent = c;
-         myRootComponent = SwingUtilities.getRoot(c);
-         myComponent.addMouseListener(this);
+
+    public MarkingPieMenuModel(Component c) {
+        sections = new ArrayList<>();
+        myComponent = c;
+        myRootComponent = SwingUtilities.getRoot(c);
+        
+        myPopup = null;
+        myPieView = new MarkingPieMenuViewTest(INNERCIRCLESIZE, OUTERCIRCLESIZE, sections);
+        
+        myComponent.addMouseListener(this);
         myComponent.addMouseMotionListener(this);
-         myPopup = null;
-         myPieView = new MarkingPieMenuViewTest(INNERCIRCLESIZE, OUTERCIRCLESIZE, sections);
-        state = STATEMENU.NOMENU;     
+        
+        myPieView.addMouseListener(this);
+        myPieView.addMouseMotionListener(this);
+        
+        state = STATEMENU.IDLE;
     }
-     
-    public void addSection(String n, Color c){
-        sections.add(new Section(-1,(float)0.0,c,n));
+
+    public void addSection(String n, Color c) {
+        sections.add(new Section(-1, (float) 0.0, c, n));
         int nbSection = sections.size();
 
         if (nbSection > 1) {
             float interval = (float) (1.0 / (nbSection));
             float angle = interval;
-            
+
             String msg = "new PIE : ";
-             for (int i=0; i<nbSection; ++i) {
-                 Section s = sections.get(i);
-                 s.setAngle(angle);
-                 s.setNumber(i);
-                 msg += "[" + angle + "] - ";
-                 angle += interval;
-             }
-             printDebug(msg);
+            for (int i = 0; i < nbSection; ++i) {
+                Section s = sections.get(i);
+                s.setAngle(angle);
+                s.setNumber(i);
+                msg += "[" + angle + "] - ";
+                angle += interval;
+            }
+            printDebug(msg);
         }
     }
 
@@ -97,214 +105,256 @@ public class MarkingPieMenuModel implements MouseMotionListener, MouseListener{
         printDebug("HIDE");
     }
     
+    private void highlight() {
+        
+    }
+
     private void showMenu() {
         int mx = gMouse.getLocationOnScreen().x - OUTERCIRCLESIZE;
         int my = gMouse.getLocationOnScreen().y - OUTERCIRCLESIZE;
         myPieView.setMouseX(mx);
         myPieView.setMouseY(my);
         myPopup = PopupFactory.getSharedInstance().getPopup(gMouse.getComponent(), myPieView, mx, my);
-       
+
         myPopup.show();
-       printDebug("SHOW MENU");
+        printDebug("SHOW MENU");
     }
 
     private void checkIfIn(MouseEvent e) {
-        double d = firstPoint.distance(e.getX(), e.getY());
+        double d = firstPoint.distance(e.getLocationOnScreen().x, e.getLocationOnScreen().y);
         if (d > INNERCIRCLESIZE && d < OUTERCIRCLESIZE) {
             if (!in) {
-            printDebug(state + " - CHECK : IN ! " + d);
-            in = true;
+                printDebug(state + " - CHECK : IN ! " + d);
+                in = true;
             }
         } else {
             if (in) {
-            printDebug(state + " - CHECK : out ..." + d);
-            in = false;
+                printDebug(state + " - CHECK : out ..." + d);
+                in = false;
             }
         }
     }
 
     private void findSection(MouseEvent e) {
-        float angle = getAngle(firstPoint, new Point(e.getX(),e.getY()));
-        selectedSection = sections.get(sections.size()-1);
-        float bot = (float) 0.0; 
-       for (Section s : sections) {
+        float angle = getAngle(firstPoint, new Point(e.getX(), e.getY()));
+        selectedSection = sections.get(sections.size() - 1);
+        float bot = (float) 0.0;
+        for (Section s : sections) {
             if (angle > bot && angle <= s.getAngle()) {
                 selectedSection = s;
             }
             bot = s.getAngle();
         }
-       
-       /* Graphics g = e.getComponent().getGraphics();
-        g.setColor(selectedSection.getColor());
-        g.fillRect(e.getX(), e.getY(), 5, 5);*/
+
+        /* Graphics g = e.getComponent().getGraphics();
+         g.setColor(selectedSection.getColor());
+         g.fillRect(e.getX(), e.getY(), 5, 5);*/
     }
-    
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
-    public void startTimer()
-    {
-        printDebug("WAITING ...");
+    public void startTimer() {
+        printDebug("MARKING WAITING ...");
         t = new Timer();
         t.schedule(new TimerTask() {
-            
+
             @Override
             public void run() {
-                
+
                 switch (state) {
-            case NOMENU: 
-                break;
-            case WAIT:
-                showMenu();
-                state = STATEMENU.PIESHOW;
-                break;
-            case PIESHOW: 
-                break;
-            case PIEHIDE: 
-                break;
-        }
-            }            
+                    case IDLE:
+                        break;
+                    case MARKING:
+                        showMenu();
+                        state = STATEMENU.SHOW;
+                        break;
+                    case SHOW:
+                        break;
+                    case HIDE:
+                        break;
+                    case MENU:
+                        break;
+                }
+            }
         }, WAITTIME);
     }
-    
+
     @Override
     public void mousePressed(MouseEvent e) {
         gMouse = e;
-            printDebug("click : " + e.getButton());
-            
+        printDebug("click : " + e.getButton());
+
         switch (state) {
-            case NOMENU:
-                /*Graphics g = e.getComponent().getGraphics();
-                g.drawLine(e.getX()-3, e.getY(), e.getX()+3, e.getY());
-                g.drawLine(e.getX(), e.getY()-3, e.getX(), e.getY()+3);*/
-                in = false;
-                firstPoint = new Point(e.getX(), e.getY());
+            case IDLE:
+                // clic gauche
                 if (e.getButton() == 3) {
-                startTimer();
-                state = STATEMENU.WAIT;
-                } else if (e.getButton() == 1){
-                    showMenu();
-                    state = STATEMENU.PIESHOW;
+                    in = false;
+                    firstPoint = new Point(e.getLocationOnScreen().x, e.getLocationOnScreen().y);
+                    startTimer();
+                    state = STATEMENU.MARKING;
                 }
                 break;
-            case WAIT:
+            case MARKING:
                 throw new RuntimeException("Mouse Down : WAIT");
-                
-            case PIESHOW:
+            case SHOW:
                 throw new RuntimeException("Mouse Down : PIESHOW");
-                
-            case PIEHIDE: 
+            case HIDE:
                 throw new RuntimeException("Mouse Down : PIEHIDE");
-                
+            case MENU:
+
         }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         /*Graphics g = e.getComponent().getGraphics();
-        g.clearRect(0, 0, 500, 500);*/
+         g.clearRect(0, 0, 500, 500);*/
         try {
             t.cancel();
-        } catch (Exception exp) {}
+        } catch (Exception exp) {
+        }
         switch (state) {
-            case NOMENU: 
+            case IDLE:
                 break;
-            case WAIT: 
+            case MARKING:
                 in = false;
-                state = STATEMENU.NOMENU;
+                state = STATEMENU.MENU;
+                showMenu();
                 break;
-            case PIESHOW:
-                if (in){
+            case SHOW:
+                if (in) {
                     doCommand();
                 }
                 hideMenu();
-                state = STATEMENU.NOMENU;
+                state = STATEMENU.IDLE;
                 break;
-            case PIEHIDE:
+            case HIDE:
                 doCommand();
-                state = STATEMENU.NOMENU;
+                state = STATEMENU.IDLE;
+                break;
+            case MENU:
+                if (e.getButton() == 1) {
+                    if (in) {
+                        doCommand();
+                    }
+                    hideMenu();
+                    state = STATEMENU.IDLE;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        switch (state) {
+            case IDLE:
+                break;
+            case MARKING:
+                checkIfIn(e);
+                if (in) {
+                    state = STATEMENU.HIDE;
+                } else {
+                    state = STATEMENU.MARKING;
+                }
+                break;
+            case SHOW:
+                checkIfIn(e);
+                if (in) {
+                    findSection(e);
+                    highlight();
+                }
+                state = STATEMENU.SHOW;
+                break;
+            case HIDE:
+                checkIfIn(e);
+                if (in) {
+                    findSection(e);
+                    highlight();
+                }
+                state = STATEMENU.HIDE;
+                break;
+            case MENU:
+                checkIfIn(e);
+                if (in) {
+                    findSection(e);
+                    highlight();
+                }
+                state = STATEMENU.MENU;
                 break;
         }
     }
     
-    @Override
-    public void mouseDragged(MouseEvent e) {
-       switch (state) {
-            case NOMENU: 
+      @Override
+    public void mouseMoved(MouseEvent e) {
+        switch (state) {
+            case IDLE:
                 break;
-            case WAIT:
-                checkIfIn(e);
-                if(in) {
-                    state = STATEMENU.PIEHIDE;
-                } else {
-                    state = STATEMENU.WAIT;
-                }
+            case MARKING:
                 break;
-            case PIESHOW: 
+            case SHOW:
+                break;
+            case HIDE:
+                break;
+            case MENU:
                 checkIfIn(e);
-                if(in) {
+                if (in) {
                     findSection(e);
+                    highlight();
                 }
-                state = STATEMENU.PIESHOW;
+                state = STATEMENU.MENU;
                 break;
-            case PIEHIDE: 
-                checkIfIn(e);
-                if(in) {
-                    findSection(e);
-                }
-                state = STATEMENU.PIEHIDE;
-                break;
-        }  
+        }
     }
     
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
-    private float getAngle(Point p1, Point p2) {
-			int mouseX = p2.x - p1.x;
-			int mouseY = p2.y - p1.y;
-			double l = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
-			double lx = mouseX / l;
-			double ly = mouseY / l;
-			double theta;
-			if (lx > 0) {
-				theta = Math.atan(ly / lx);
-			} else if (lx < 0) {
-				theta = -1 * Math.atan(ly / lx);
-			} else {
-				theta = 0;
-			}
+  
 
-			if ( (mouseX > 0) && (mouseY < 0)) {
-				theta = -1 * theta;
-			} else if (mouseX < 0) {
-				theta += Math.PI;
-			} else {
-				theta = 2 * Math.PI - theta;
-			}
-
-			return (float) (theta / (2 * Math.PI));              
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-       
-    }
-    
     @Override
     public void mouseClicked(MouseEvent e) {
-        
     }
+
+
+    private float getAngle(Point p1, Point p2) {
+        int mouseX = p2.x - p1.x;
+        int mouseY = p2.y - p1.y;
+        double l = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
+        double lx = mouseX / l;
+        double ly = mouseY / l;
+        double theta;
+        if (lx > 0) {
+            theta = Math.atan(ly / lx);
+        } else if (lx < 0) {
+            theta = -1 * Math.atan(ly / lx);
+        } else {
+            theta = 0;
+        }
+
+        if ((mouseX > 0) && (mouseY < 0)) {
+            theta = -1 * theta;
+        } else if (mouseX < 0) {
+            theta += Math.PI;
+        } else {
+            theta = 2 * Math.PI - theta;
+        }
+
+        return (float) (theta / (2 * Math.PI));
+    }
+
     @Override
     public void mouseEntered(MouseEvent e) {
-        
+
     }
+
     @Override
     public void mouseExited(MouseEvent e) {
-        
+
     }
-    
+
     private void printDebug(String msg) {
-        if (DEBUGMODE) System.out.println(msg);
+        if (DEBUGMODE) {
+            System.out.println(msg);
+        }
     }
-    
+
 }
